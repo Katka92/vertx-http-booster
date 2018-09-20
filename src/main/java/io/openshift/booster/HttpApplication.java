33de502ba8edaf9625b0eacf0849ba63cb1466e5
test-user <test@mail.com>
2018-09-20 11:15:15 +0000

    rebqerbg

--- src/main/java/io/openshift/booster/HttpApplication.java
+++ src/main/java/io/openshift/booster/HttpApplication.java
@@ -11,7 +11,7 @@ import static io.vertx.core.http.HttpHeaders.CONTENT_TYPE;
 
 public class HttpApplication extends AbstractVerticle {
 
-  protected static final String template = "Bonjour, %s!";1537441329997
+  protected static final String template = "Bonjour, %s!";1537441329997 qrgqregqerg qrgq
 
   @Override
   public void start(Future<Void> future) {
